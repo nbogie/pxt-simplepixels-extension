a3a26d9dadc62e54bf400801d63ad8b0133feb11
nbogie <neillbogie@googlemail.com>
2019-10-04 23:25:11 +0100

    added more effects.

--- custom.ts
+++ custom.ts
@@ -4,6 +4,7 @@
 //% weight=100 color=#0fbc11 icon=""
 namespace simplePixels {
     //% block="make %strip spin one %color=neopixel_colors pixel"
+    //% color.shadow="random_color"
     export function effectSpin(strip: neopixel.Strip, color: number): void {
         strip.clear()
         strip.setPixelColor(0, color)
@@ -16,7 +17,7 @@ namespace simplePixels {
         strip.clear()
         strip.show()
     }
-    //% block="make %strip change colour according to tilt" 
+    //% block="change %strip colour when tilted" 
     export function effectColorByTilt(strip: neopixel.Strip) {
         strip.clear()
         for (let i = 0; i < 100; i++) {
@@ -33,7 +34,8 @@ namespace simplePixels {
         strip.show()
     }
 
-    //% block="make %strip spin one %color=neopixel_colors pixel, quickly"
+    //% block="%strip spin one %color=neopixel_colors pixel, quickly"
+    //% color.shadow="random_color"
     export function effectFastSpin(strip: neopixel.Strip, color: number) {
         strip.clear()
         for (let passIx2 = 0; passIx2 <= 10; passIx2++) {
@@ -58,10 +60,57 @@ namespace simplePixels {
     }
 
     function wipe(strip: neopixel.Strip) {
-        strip.clear();
-        strip.show();
+        for (let i = 0; i < 24; i++) {
+            strip.shift(1)
+            strip.show()
+            basic.pause(10)
+        }
+    }
+    
+    //% block="%strip sparkle randomly with %color=neopixel_colors"
+    //% color.shadow="random_color"
+    export function effectSparkle(strip: neopixel.Strip, color: number) {
+        strip.clear()
+        strip.setBrightness(255)
+        for (let i = 0; i < 48; i++) {
+            basic.pause(50)
+            strip.clear()
+            strip.setPixelColor(Math.randomRange(0, strip.length() - 1), color)
+        }
+        wipe(strip)
+    }
+
+    //% block="Spin trio on %strip with colors %color1=neopixel_colors pixels %color2=neopixel_colors pixels"
+    //% color1.shadow="random_color"
+    //% color2.shadow="random_color"
+    export function effectSpinTriangle2(strip: neopixel.Strip, color1: number, color2: number) {
+        strip.clear()
+        strip.setBrightness(255)
+        strip.setPixelColor(1, color1)
+        strip.setPixelColor(8, color1)
+        strip.setPixelColor(16, color1)
+        strip.setBrightness(30)
+        strip.setPixelColor(0, color2)
+        strip.setPixelColor(2, color2)
+        strip.setPixelColor(7, color2)
+        strip.setPixelColor(9, color2)
+        strip.setPixelColor(15, color2)
+        strip.setPixelColor(17, color2)
+        for (let i = 0; i < 100; i++) {
+            basic.pause(50)
+            strip.rotate(0 - pins.map(
+                input.rotation(Rotation.Roll),
+                -180,
+                180,
+                -5,
+                5
+            ))
+            strip.show()
+        }
+        wipe(strip)
     }
     //% block="make %strip spin three %color=neopixel_colors pixels" 
+    //% color1.shadow="random_color"
     export function effectSpinThree(strip: neopixel.Strip, color: number): void {
         strip.clear()
         strip.setPixelColor(0, color)
@@ -82,7 +131,7 @@ namespace simplePixels {
         strip.show()
     }
     //% block="make |%strip| glow|color %color=neopixel_colors" 
-
+    //% color.shadow="random_color"
     export function effectGlow(strip: neopixel.Strip, color: number) {
         let myBrightness = 0
         for (let i = 0; i < 2; i++) {
@@ -101,7 +150,7 @@ namespace simplePixels {
         }
         strip.setBrightness(255)
     }
-    //% block
+    //% blockId="random_color" block
     export function randomColor(): number {
         return neopixel.hsl(Math.randomRange(0, 360), 99, 50)
     }
